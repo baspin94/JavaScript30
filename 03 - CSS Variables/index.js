1787fc02b8731627e46c6841c3ddb36fda90734a
Bianca Aspin <bianca@mj94.net>
2024-01-26 22:41:50 -0800

    Add post-video solution.

--- 03 - CSS Variables/index.js
+++ 03 - CSS Variables/index.js
@@ -1,32 +1,9 @@
-const spacingInput = document.getElementById("spacing")
-const blurInput = document.getElementById("blur")
-const baseInput = document.getElementById("base")
-
-// function handleSpacing(e) {
-//     console.log(e.target.id)
-//     document.body.style.setProperty('--spacing', `${e.target.value}px`)
-// }
-
-// function handleBlur(e) {
-//     console.log(e)
-//     document.body.style.setProperty('--blur', `${e.target.value}px`)
-// }
-
-// function handleBase(e) {
-//     console.log(e)
-//     document.body.style.setProperty('--base-color', e.target.value)
-// }
-
 function handleChange(e) {
     id = e.target.id
     value = e.target.value
-    if (id === "base") {
-        document.body.style.setProperty(`--${id}`, value)
-    } else {
-        document.body.style.setProperty(`--${id}`, `${value}px`)
-    }
+    suffix = e.target.dataset.sizing || '';
+    document.body.style.setProperty(`--${id}`, value + suffix)
 }
 
-spacingInput.addEventListener("input", (e) => handleChange(e))
-blurInput.addEventListener("input", (e) => handleChange(e))
-baseInput.addEventListener("input", (e) => handleChange(e))
+const inputs = document.querySelectorAll(".controls input")
+inputs.forEach(input => input.addEventListener("input", (e) => handleChange(e)))
